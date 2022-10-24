978587c6d926fb5930e3ad20353a63513048a254
Keshav S <s.keshav13142@gmail.com>
2022-10-24 17:46:29 +0530

    Added files controller

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,8 +25,8 @@ const Navbar = () => {
       {user && (
         <div className="flex-none gap-2">
           <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar ">
+              <div className="w-10 rounded-full ring ring-opacity-50 ring-purple-700">
                 <img src="https://placeimg.com/80/80/people" />
               </div>
             </label>
