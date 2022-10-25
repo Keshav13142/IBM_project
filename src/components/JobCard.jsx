aaa6b4afd0bafd7315c5da2c5c24e04650045e1c
rmkcet2k22 <dheerajhemachandran@gmail.com>
2022-10-25 18:46:20 +0530

    dashboard api

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const JobCard = () => {
+const JobCard = ({title,company,description,link}) => {
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div class="max-w-sm flex flex-col rounded overflow-hidden shadow-lg">
   <div class="px-6 py-4">
-    <div class="font-bold text-xl">job title</div>
-    <div className="text-lg mb-2 text-gray-400">company</div>
-    <p class="text-gray-800 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    <div class="font-bold text-xl">{title}</div>
+    <div className="text-lg mb-2 text-gray-400">{company}</div>
+    <p class="text-ellipsis overflow-hidden text-gray-800 text-base">
+      {description}
     </p>
   </div>
-  <div class="px-6 pt-4 pb-2 mb-2">
-  <a class="bg-transparent hover:bg-primary text-primary font-semibold hover:text-white py-2 px-4 border border-primary hover:border-transparent rounded">
+  <div class="px-6 pt-4 pb-2 mt-auto mb-2">
+  <a href={link} target="__blank" class="bg-transparent hover:bg-primary text-primary font-semibold hover:text-white py-2 mb-0 mt-4 px-4 border border-primary hover:border-transparent rounded">
   apply
 </a>
 </div>
--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,39 +1,55 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect,useState } from "react";
+import axios from "axios";
 import { AppContext } from "../context/AppContext";
 import JobCard from "../components/JobCard";
-import Links from "../components/Links";
+import Skill from "../components/Skill";
 import SearchBar from "../components/SearchBar";
 
 const Dashboard = () => {
   const { user, setUser } = useContext(AppContext);
 
+  const skills=["python","aws"]
+  
+  const [query, setquery] = useState(null)
+  const id="d0011a4d"
+  const key="93ce99da369260830b2640af5cf56923"
+  const baseURL = `http://api.adzuna.com/v1/api/jobs/in/search/1?app_id=${id}&app_key=${key}&results_per_page=100&what=${query}&what_and=${skills}&content-type=application/json`;
+
+  const [posts, setposts] = useState(null);
+
   useEffect(() => {
-    console.log(user, "From Dashboard");
-  }, []);
+    axios.get(baseURL).then((response) => {
+      const data=response.data
+      setposts(data.results)
+  })
+}, [query])
+
 
   return(
   <div className="flex gap-10 my-10 lg:my-24 mx-20 lg:mx-40">
     <div className="hidden lg:block bg-primary w-1/5 p-10 h-3/6 rounded-lg">
-      <div className="text-xl text-white capitalize font-extrabold mb-6">Top jobs</div>
-    <ul className="list-none text-gray-400 underline flex flex-col gap-2">
-        <Links/>
-        <Links/>
-        <Links/>
-        <Links/>
-        <Links/>
+      <div className="text-xl text-white capitalize font-extrabold mb-6">Your skills</div>
+    <ul className="list-none text-gray-200 flex flex-col gap-2">
+        {skills.map((skill)=>
+        <Skill skill={skill}/> 
+        )}
       </ul>
+      <button className="p-2 bg-white text-primary rounded mt-5"><a href="/profile">Change Skills</a></button>
     </div>
 
     <div className="mx-auto">
-      <SearchBar/>
+      <SearchBar setquery={setquery} />
       
       <div className="mt-10 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2">
 
 
-     
-      <JobCard/>
-      <JobCard/>
-      <JobCard/>
+     {query!==null?
+     (posts.map((post)=>
+        <JobCard title={post.title} company={post.company.display_name} description={post.description} link={post.redirect_url} />
+     ))
+     :
+     <>
+    </>}
       </div>
     </div>
 
